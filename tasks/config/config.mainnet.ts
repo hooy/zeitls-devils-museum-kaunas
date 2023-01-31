dfa86fddb7e65059ec994e1878dc772922598e07
Sergey Gorbunov <info@maxwell.lv>
2023-01-31 10:27:16 +0200

    Deployed PROD values

--- tasks/config/config.mainnet.ts
+++ tasks/config/config.mainnet.ts
@@ -1,10 +1,10 @@
 export default {
     openseaRegistry: "0xa5409ec958C83C3f309868babACA7c86DCB077c1",
-    owner: "0x7AeA2070ec1be39Df1F67A58dbA6ce097442b082",
-    maintainer: "0x0CbD644c7B56d6B075b51bA080BC350d1dEDFcfa",
-    weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-    whitelistUri: "ipfs://QmZRn8pQCACNCWTydUZ62oNtEHbdnS3enxTkWEYXDjfxzj/",
-    signerKYC: "0x810cdfA7D49eC5a6C21DE02ECFbbA7A1ad64a76d",
+    owner: "0x7AeA2070ec1be39Df1F67A58dbA6ce097442b082",                    // checked 
+    maintainer: "0x0CbD644c7B56d6B075b51bA080BC350d1dEDFcfa",               // gnosis, 
+    weth: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",                     // wrapped ETH, checked
+    whitelistUri: "ipfs://QmZRn8pQCACNCWTydUZ62oNtEHbdnS3enxTkWEYXDjfxzj/", // checked
+    signerKYC: "0x810cdfA7D49eC5a6C21DE02ECFbbA7A1ad64a76d",                // checked
     timeBuffer: 30 * 60, // 30 min
     duration: 72 * 60 * 60, // 72 hour
     minBidDiff: 5, // 5%
